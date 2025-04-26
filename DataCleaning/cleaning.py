d4bde76138f2f0019c3d31824e426bbb05d9925f
Henry Velasquez <otshenry6@gmail.com>
2025-04-26 19:22:21 -0400

    incidents csv file

--- DataCleaning/cleaning.py
+++ DataCleaning/cleaning.py
@@ -1,1 +1,18 @@
 
+import json 
+import pandas as pd
+from datetime import datetime
+
+with open('/Users/henryvelasquez/RamsHackathon/RamsHackathon/Copy of RAMHACK CASEFILES 3/incident_reports.json', 'r') as f:
+    incident_reports = json.load(f)
+incident_reports_df = pd.json_normalize(incident_reports)
+incident_reports_df.to_csv('cleaned_incident_reports.csv', index=False)
+print(incident_reports_df)
+
+with open('/Users/henryvelasquez/RamsHackathon/RamsHackathon/Copy of RAMHACK CASEFILES 3/phone_pings.json') as f:
+    phone_pings = json.load(f)
+print(phone_pings)
+
+with (open('/Users/henryvelasquez/RamsHackathon/RamsHackathon/Copy of RAMHACK CASEFILES 3/suspects.json')) as f:
+    suspects = json.load(f)
+print(suspects)
