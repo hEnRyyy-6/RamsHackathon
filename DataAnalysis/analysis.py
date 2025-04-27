7dda32568c5f06102215484ed989896f7dd70e71
Jordan Tisdol <jordan.tisdol@gmail.com>
2025-04-27 14:10:55 -0400

    Initial commit

--- DataAnalysis/analysis.py
+++ DataAnalysis/analysis.py
@@ -1,5 +1,13 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-print("hello")
+
+import pandas as pd
+
+
+incident_reports_df = pd.read_csv('/Users/henryvelasquez/RamsHackathon/RamsHackathon/cleaned_incident_reports.csv')
+
+
+print(incident_reports_df.head())
+
+print(incident_reports_df.describe())
+print(incident_reports_df.info())
+
